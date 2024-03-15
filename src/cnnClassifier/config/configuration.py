eb6578445e98dc8925fe42bac5e22275817bc879
Siamias202 <kawsarahamed@iut-dhaka.edu>
2024-03-16 00:31:00 +0600

    added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -84,7 +84,7 @@ class ConfigurationManager:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/Covid19-dataset",
-            mlflow_uri="https://dagshub.com/entbappy/Covid19-Detection.mlflow",
+            mlflow_uri="https://dagshub.com/Siamias202/Covid19-Detection",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
